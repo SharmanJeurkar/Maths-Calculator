c5522b565dfa7c25556ac35d08f823363f8b6dec
“SharmanJeurkar” <kirtisjeurkar@gmail.com>
2022-02-11 13:12:48 +0530

    added the code to push the enterd values to add.py class

--- askUser.py
+++ askUser.py
@@ -1,6 +1,9 @@
 import add
 
 def askUserNumbers():
-     print("hello world")
+      num1 = int(input("Enter number 1: ")) 
+      num2 = int(input("Enter number 2: "))
+
+      add.add(num1,num2)
 
 askUserNumbers()
