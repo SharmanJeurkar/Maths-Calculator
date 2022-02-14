dfcfa01eb6c40a16ca33a594f7701f95cfaad85d
“SharmanJeurkar” <kirtisjeurkar@gmail.com>
2022-02-14 14:58:56 +0530

    added method for subtraction

--- askUser.py
+++ askUser.py
@@ -1,9 +1,17 @@
 import add
+import subtract
 
-def askUserNumbers():
+def askUserToAdd():
       num1 = int(input("Enter number 1: ")) 
       num2 = int(input("Enter number 2: "))
 
       add.add(num1,num2)
 
-askUserNumbers()
+def askUserToSubtract():
+      num1 = int(input("Enter number 1: ")) 
+      num2 = int(input("Enter number 2: "))
+
+      subtract.subtract(num1,num2)
+
+askUserToSubtract()
+askUserToAdd()
